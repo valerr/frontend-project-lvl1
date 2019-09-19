8033a4d0739ab334306c4e99631b25c0f16dffae
VV <spokich394@gmail.com>
2019-09-19 17:28:03 +0300

    add progression game

---
+++ src/bin/brain-progression.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import playProgression from '../game-progression';
+
+playProgression();
---
+++ src/game-progression.js
@@ -0,0 +1,32 @@
+import { getRandomInt } from './game-even';
+import launcher, { quesAns } from './index';
+
+const description = 'What number is missing in the progression?';
+const progrLength = 10;
+
+const makeProgression = (firstNum, diff) => {
+  const progression = [];
+
+  for (let i = 0; i < progrLength; i += 1) {
+    progression[i] = firstNum + diff * i;
+  }
+  return progression;
+};
+
+const makeGameData = () => {
+  const num1 = getRandomInt(1, 50);
+  const step = getRandomInt(1, 10);
+  const progression = makeProgression(num1, step);
+  const hiddenInd = getRandomInt(1, progrLength);
+
+  const gameAnswer = String(progression[hiddenInd]);
+  progression[hiddenInd] = '..';
+  const question = progression.join(' ');
+
+  console.log(`Question: ${question}`);
+
+  return quesAns(question, gameAnswer);
+};
+
+const playProgression = () => launcher(description, makeGameData);
+export default playProgression;
